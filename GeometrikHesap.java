305e363655015aa2f8e56496f8ec2dcbab00f73a
nazlikaraagac <250541105@firat.edu.tr>
2025-11-06 18:40:45 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,39 @@
+/*
+ * Ad Soyad: Nazlı Karaağaç
+ * Ogrenci No: 250541105
+ * Tarih: 06.11.2025
+ * Aciklama: Gorev 1 - Geometrik Hesaplayici
+ *
+ *Bu program bir dairenin yarıçapını kullanıcıdan alır ve dairenin alanını, hacmini, çevresini, çapını, yüzey alanını hesaplayarak çıktısını verir.
 
+import java.util.Scanner;
+
+public class GeometrikHesaplama {
+    
+    public static void main(String[] args) {
+        final String GEOMETRIK_HESAPLAYICI = "=== GEOMETRIK HESAPLAYICI ===";
+        final double PI =  3.14159;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(GEOMETRIK_HESAPLAYICI);
+
+        System.out.print("Dairenin yaricapini giriniz (cm): ");
+        double yaricap = input.nextDouble();
+
+        double alan = PI * yaricap * yaricap;
+        double cevre = 2 * PI * yaricap;
+        double cap = 2 * yaricap;
+        double hacim = (4.0 / 3.0) * PI * yaricap * yaricap * yaricap;
+        double yuzeyAlani = 4 * PI * yaricap * yaricap;
+       
+        System.out.println("\n" + GEOMETRIK_HESAPLAYICI);
+        System.out.printf("Daire Alani: %.2f cm^2%n", alan);
+        System.out.printf("Daire Cevresi: %.2f cm%n", cevre);
+        System.out.printf("Daire Capi: %.2f cm%n", cap);
+        System.out.printf("Kure Hacmi: %.2f cm^3%n", hacim);
+        System.out.printf("Kure Yuzey Alani: %.2f cm^2%n", yuzeyAlani);
+      
+        input.close();
+    }
+}
